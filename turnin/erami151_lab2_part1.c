96e53af9fef9fc41dc830b76733b6167ecade5c7
estela-ramirez <ramirezestela01@gmail.com>
2021-01-14 23:53:13 -0800

    Completed part 1

---
+++ turnin/erami151_lab2_part1.c
@@ -0,0 +1,41 @@
+/*	Author: Estela Ramirez Ramirez
+ *  Partner(s) Name: 
+ *	Lab Section:21
+ *	Assignment: Lab 2  Exercise 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	// = 0xF0, sets hight bits to outputs and low bits to inputs
+	// PORTA - write only, PINA - readonly
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A for A0
+    unsigned char tmpA2 = 0x00; // Temporary varuable to hold the value of A for A1
+while(1) {
+        // 1) Read input
+        tmpA = PINA & 0x01;
+	tmpA2 = PINA & 0x02;  
+        // 2) Perform computation
+        // if PA0 is 1 and PA0 is 0, set PB0 = 1, else = 0
+        if (tmpA == 0x01 && tmpA2 != 0x02) { // True if PA0 is 1 and PA1 is not 1
+            tmpB = (tmpB & 0xFE) | 0x01; // Sets tmpB to bbbbbbb1
+                             // (clear rightmost  bit, then set to 1)
+        } else {
+            tmpB = (tmpB & 0xFE) | 0x00; // Sets tmpB to bbbbbbb0
+                             // (clear rightmost bit, then set to 0)
+        }    
+    // 3) Write output
+PORTB = tmpB;    
+    }
+    return 0;
+}
+
